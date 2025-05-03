7a6581282ec0b683f3a9021e14ce65a4a6fb95c7
Louise Morais <louisemorais3@gmail.com>
2025-05-03 20:28:30 -0300

    feat: subindo o config

---
+++ src/main/java/br/senai/lab364/futurodev/springsec/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package br.senai.lab364.futurodev.springsec.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/login").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/organizations").hasAnyAuthority(
+                                "ADMIN", "USER"
+                        )
+                        .requestMatchers("/organizations", "/users").hasAuthority("ADMIN")
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+}
